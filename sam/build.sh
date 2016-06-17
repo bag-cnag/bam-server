ed83ecf23bf47ae6f031c947972557e28de21ee8
frasator <frasator@gmail.com>
2016-06-17 17:45:03 +0200

    updated build

--- sam/build.sh
+++ sam/build.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 VERSION=1.3.1
 FILE=samtools-$VERSION.tar.bz2
-FILE_CHECKSUM=6c3d74355e9cf2d9b2e1460273285d154107659efa36a155704b1e4358b7d67e
 FOLDER=samtools-$VERSION
 PREFIX_FOLDER=$(pwd)/sam/samtools
-
 
 function build() {
   tar xvfj $FILE
@@ -16,12 +14,8 @@ function build() {
 }
 
 rm -rf sam/samtools
-CHECKSUM=$(sha256sum sam/$FILE | cut -d " " -f1)
-if [ "$FILE_CHECKSUM" -ne "$CHECKSUM" ]; then
-    rm -rf $FILE
-    wget https://github.com/samtools/samtools/releases/download/$VERSION/$FILE
-fi
+rm -rf sam/$FILE
+cd sam/
+wget https://github.com/samtools/samtools/releases/download/$VERSION/$FILE
 
-cd sam
 build
-rm -rf
